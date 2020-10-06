f62ac787408ef3b20ed3f2cfafb9102774af367c
Michael Pieper <michaelepieper@gmail.com>
2020-10-05 23:41:14 -0500

    Use props on GameCard, add styling, use PropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Link} from "react-router-dom";
-import './App.css';
-import GameCard from "./components/GameCard";
+import './App.scss';
+import GameCard from "./components/GameCard/GameCard";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -13,7 +13,10 @@ function App() {
                 <Link to="/more">More</Link>
                 <Link to="/login">Log in</Link>
             </Navbar>
-            <GameCard/>
+            <h1 style={{color: "white"}}>Game Catalog</h1>
+            <GameCard name="Agar.io" description="Play as a circle on a large grid, collect dots, and eat others to grow" updated="10/5/2020" color="#e55039"/>
+            <GameCard name="Slither.io" description="Slither around as a snake, don't run into anyone else's tails" updated="10/5/2020" color="#38ada9"/>
+            <GameCard name="Paper.io" description="Glide across the room as a block, claiming territory and stealing it from others" updated="10/5/2020" color="#4a69bd"/>
         </BrowserRouter>
     </div>
   );
