0b9d8bb0a5dcca790d124501a0c29e65231af3ce
Michael Pieper <michaelepieper@gmail.com>
2020-10-07 17:28:47 -0500

    Render different routes based on URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {BrowserRouter, Link} from "react-router-dom";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
 import './App.scss';
-import GameCard from "./components/GameCard/GameCard";
 import Navbar from "./components/Navbar/Navbar";
+import GameCatalog from "./views/GameCatalog";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
                 <Link to="/more">More</Link>
                 <Link to="/login">Log in</Link>
             </Navbar>
-            <h1 style={{color: "white"}}>Game Catalog</h1>
-            <GameCard name="Agar.io" description="Play as a circle on a large grid, collect dots, and eat others to grow" updated="10/5/2020" color="#e55039"/>
-            <GameCard name="Slither.io" description="Slither around as a snake, don't run into anyone else's tails" updated="10/5/2020" color="#38ada9"/>
-            <GameCard name="Paper.io" description="Glide across the room as a block, claiming territory and stealing it from others" updated="10/5/2020" color="#4a69bd"/>
+            <Switch>
+                <Route exact={true} path="/" component={GameCatalog}/>
+                <Route exact={true} path="/games" component={GameCatalog}/>
+            </Switch>
         </BrowserRouter>
     </div>
   );
