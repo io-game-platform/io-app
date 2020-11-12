7f88159bab310f29122367b02c2ca22898a71ef4
Steven Lu <sylfw6@mst.edu>
2020-11-11 22:05:09 -0600

    Changed formatting and removed unnecssary code

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
         <BrowserRouter>
             <Navbar>
                 <Link to="/games">Games</Link>
-                <Link to="/more">More</Link>
+                <Link to="/templates">Templates</Link>
                 <Link to="/login">Log in</Link>
             </Navbar>
             <Switch>
                 <Route exact={true} path="/" component={GameCatalog}/>
                 <Route exact={true} path="/games" component={GameCatalog}/>
-                <Route exact={true} path="/server" component={GameServer}/>
+                <Route exact={true} path="/new-template" component={GameServer}/>
                 <Route exact={true} path="/templates" component={GameTemplates}/>
                 <Route path="/games/:gameId" render={({ match }) => {
                     const gameId = parseInt(match.params.gameId);
--- src/views/GameTemplates/GameTemplates.js
+++ src/views/GameTemplates/GameTemplates.js
@@ -1,57 +1,57 @@
 import React, {Component, Fragment} from "react";
 import Button from "../../components/Button/Button";
-//import PropTypes from "prop-types";
-//import {apiFetch} from "../../utils";
 import "./GameTemplates.scss";
-//import { Link } from "react-router-dom";
 
 class GameTemplates extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            game: null
+            templates: []
         }
     }
 
     render() {
 		return (
 			<Fragment>
-				<div className='template-header'>
-                    <p>Templates</p>
-                    <div className='input-field'>
-                        <input placeholder='Filter'/>
+                <div className='page-container'>
+                    <div className='template-header'>
+                        <Button className='new-template-button'>New Template</Button>
+                        <h1>Templates</h1>
+                        <div className='input-field'>
+                            <input placeholder='Filter'/>
+                        </div>
                     </div>
-                </div>
-                <div className='my-server1'>
-                    <div className='server-info'>
-                        <h1>My Agar.io Server</h1>
-                        <h2>Agar.io - 0 Players</h2>
+                    <div className='my-server'>
+                        <div className='server-info'>
+                            <h1>My Agar.io Server</h1>
+                            <h2>Agar.io - 0 Players</h2>
+                        </div>
+                        <div className='server-buttons'>
+                            <Button className='open-server'>Open</Button> <Button className='edit-server'>Edit</Button> 
+                        </div>
                     </div>
-                    <div className='server-buttons'>
-                        <Button className='open-server'>Open</Button> <Button className='edit-server'>Edit</Button> 
-					</div>
-                </div>
-                <div className='my-server2'>
-                    <div className='server-info'>
-                        <h1>My Paper.io Server</h1>
-                        <h2>Paper.io - 0 Players</h2>
+                    <div className='my-server'>
+                        <div className='server-info'>
+                            <h1>My Paper.io Server</h1>
+                            <h2>Paper.io - 0 Players</h2>
+                        </div>
+                        <div className='server-buttons'>
+                            <Button className='open-server'>Open</Button> <Button className='edit-server'>Edit</Button> 
+                        </div>
                     </div>
-                    <div className='server-buttons'>
-                        <Button className='open-server'>Open</Button> <Button className='edit-server'>Edit</Button> 
-					</div>
-                </div>
-                <div className='my-server3'>
-                    <div className='server-info'>
-                        <h1>My Paper.io Server</h1>
-                        <h2>Paper.io - 0 Players</h2>
+                    <div className='my-server'>
+                        <div className='server-info'>
+                            <h1>My Paper.io Server</h1>
+                            <h2>Paper.io - 0 Players</h2>
+                        </div>
+                        <div>
+                            <p>CODE</p>
+                        </div>
+                        <div className='server-buttons'>
+                            <Button className='join-server'>Join</Button> <Button className='close-server'>Close</Button> <Button className='edit-server'>Edit</Button> 
+                        </div>
                     </div>
-                    <div>
-                        <p>CODE</p>
-                    </div>
-                    <div className='server-buttons'>
-                        <Button className='join-server'>Join</Button> <Button className='close-server'>Close</Button> <Button className='edit-server'>Edit</Button> 
-					</div>
                 </div>
 			</Fragment>
 		);
