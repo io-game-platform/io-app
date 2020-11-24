b02490387e1744d564193a9b41db01e6a46cc452
pieperm <mepww2@umsystem.edu>
2020-11-24 05:06:23 -0600

    Create logo and add it to navbar and favicon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import './Navbar.scss'
+import AppLogo from "../../app-logo.png";
 
 class Navbar extends Component {
 
     render() {
         return (
             <div className="nav">
+                <img alt="io.io" src={AppLogo}/>
                 <h1>io.io</h1>
                 {this.props.children}
             </div>
