182c940e07bb7610c2aa1d3f9b9daa256d2d1e6c
pieperm <mepww2@umsystem.edu>
2020-12-01 17:50:16 -0600

    Update table name

--- api/queries/GameData.js
+++ api/queries/GameData.js
@@ -3,12 +3,12 @@ const knex = require("../../knex");
 class GameData {
 
     static async getAllGames() {
-        let games = await knex("games").select("*");
+        let games = await knex("game").select("*");
         return games;
     }
 
     static async getGameById(id) {
-        let [game] = await knex("games").select("*").where({id: id}).limit(1);
+        let [game] = await knex("game").select("*").where({id: id}).limit(1);
         return game;
     }
 
