5766bcb831c570641a5b49016440b2efa281be82
pieperm <mepww2@umsystem.edu>
2020-11-30 22:45:27 -0600

    Set up API routes for accessing server template data

---
+++ api/routes/templates.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const TemplateData = require("../queries/TemplateData");
+
+// Get all server templates
+router.get('/', (req, res, next) => {
+    try {
+        const allTemplates = TemplateData.getAllTemplates(req, res);
+        if (!allTemplates) {
+            return res.sendStatus(404);
+        }
+        req.templates = allTemplates;
+        res.json(req.templates);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
+});
+
+router.post('/', (req, res, next) => {
+   try {
+       TemplateData.saveTemplate(req.body);
+       res.status(200).send();
+   } catch (e) {
+       console.error(e);
+       next(e);
+   }
+});
+
+module.exports = router;
