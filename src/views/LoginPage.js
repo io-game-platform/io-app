49df065de92b8b609e9070708f823daf341707c4
aydenmason <aydenjmason@gmail.com>
2020-12-02 22:08:11 -0600

    creating PULL REQUEST for new user acocunt creation. lines 11-15 on login page.js will not work and need immediate attention. Emergency meeting needs to be called.

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -4,9 +4,27 @@ import GoogleLogin from 'react-google-login'
 class LoginPage extends Component {
 
   responseGoogle=(response)=>{
-    console.log(response);
-    console.log(response.profileObj);
-  }
+    console.log(response.email);
+    var account = response.email;
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+      host: "localhost", //lines 11 to 14 need to be changed I do not know the db info
+      user: "admin", //lines 11 to 14 need to be changed I do not know the db info
+      password: "admin",//lines 11 to 14 need to be changed I do not know the db info
+      database: "test"//lines 11 to 14 need to be changed I do not know the db info
+    });
+    mysql.query("SELECT username FROM user WHERE username = '"+ account +"'", function(err, result, field){
+      if(result.length === 0){
+        //add a new user because they do not exist in the db
+        var sql = "INSERT INTO user (username,email,role,date_created,git_ssh_key,git_account) VALUES (account, NULL , player, 'now()', NULL, NULL)";
+        mysql.query(sql, function(err,result){
+        if(err) throw err;
+        console.log("user account created. finishing login.");
+        }    
+        )}
+        location.href = '/GameCatalog.js';
+    })};
+    
   render(){
     return (
       <div>
