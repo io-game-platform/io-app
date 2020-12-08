8220f15b0823bc9df86f4657f8f82e9e78132d27
pieperm <mepww2@umsystem.edu>
2020-12-07 23:43:05 -0600

    Remove lorem ipsum

--- src/views/GameDetails/GameDetails.js
+++ src/views/GameDetails/GameDetails.js
@@ -30,14 +30,14 @@ class GameDetails extends Component {
                 <div className="image-header">
                     <img className="header-image" src={GameBanner} alt="Game Banner"/>
                     <div className="header-content">
-                        <h1 className="page-title">{!!this.state.game ? this.state.game.name : "Game Title"}</h1>
+                        <h1 className="page-title">{this.state.game.title}</h1>
                         <div className="game-data">
                             <h3>0 players online</h3>
-                            <h3>Last updated 10/16/2020</h3>
+                            <h3>Last updated {this.state.game.updated}</h3>
                         </div>
                     </div>
                 </div>
-                <p className="game-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur nulla ex, ut sagittis tellus ornare ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia scelerisque nulla quis bibendum. Mauris dui mi, fringilla non eros ac, faucibus faucibus orci. Maecenas elit augue, malesuada a dignissim varius, efficitur a felis. Fusce rutrum aliquam nisl, sit amet convallis turpis blandit et. Phasellus euismod eget dolor auctor ultricies. Duis ullamcorper non elit id aliquam. Aliquam ultricies dolor vel felis convallis facilisis. In eget orci sit amet purus faucibus pretium nec vitae tortor. Mauris id eros nisl. In nec urna sollicitudin, eleifend nibh vitae, bibendum est. Fusce placerat enim non dapibus suscipit. Etiam vitae fermentum ex, vel aliquet ex.</p>
+                <p className="game-description">{this.state.game.description}</p>
                 <div className="game-mode-container">
                     <div className="game-mode-section">
                         <h2>Online</h2>
