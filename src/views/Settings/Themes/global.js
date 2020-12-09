cb75f29d62802cdb479dce3b36c0f33ce79053a9
Steven Lu <sylfw6@mst.edu>
2020-12-08 20:01:10 -0600

    Fixed old issue

--- src/views/Settings/Themes/global.js
+++ src/views/Settings/Themes/global.js
@@ -14,7 +14,7 @@ export const GlobalStyles = createGlobalStyle`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 100vh;
+    // height: 100vh;
     margin: 0;
     padding: 0;
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
